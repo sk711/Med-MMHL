e17aeb6af2fa61e04d837d1243923676b15c52fd
sk711 <55020880+sk711@users.noreply.github.com>
2024-03-20 09:56:25 +0530

    Update MyFunc.py

--- Multimodal_fake_news_benchmark_1/MyFunc.py
+++ Multimodal_fake_news_benchmark_1/MyFunc.py
@@ -9,26 +9,15 @@ from sklearn.cluster import KMeans
 import re
 
 def read_benchmark_set(save_path):
-    #tr_sv_path = os.path.join(save_path, 'train.csv')
-    #dev_sv_path = os.path.join(save_path, 'dev.csv')
-    #te_sv_path = os.path.join(save_path, 'test.csv')
-
-   tr_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/train.csv')
-   dev_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/dev.csv')
-   te_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/test.csv')
-
-    #tr_sv_path = os.path.join(save_path, 'train.csv')
-    #dev_sv_path = os.path.join(save_path, 'dev.csv')
-    #te_sv_path = os.path.join(save_path, 'test.csv')
+ 
+    tr_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/train.csv')
+    dev_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/dev.csv')
+    te_sv_path = os.path.join(save_path, 'kaggle/working/Med-MMHL/fakenews_article/test.csv')
     
     tr_df = pd.read_csv(tr_sv_path, header=0, sep=',')
     dev_df = pd.read_csv(dev_sv_path, header=0, sep=',')
     te_df = pd.read_csv(te_sv_path, header=0, sep=',')
 
-    # if 'source' in tr_df:
-    #     tr_df = tr_df.sample(frac=0.2)  # shuffle operation
-    #     dev_df = dev_df.sample(frac=0.2)  # shuffle operation
-    #     te_df = te_df.sample(frac=0.2)  # shuffle operation
     print('Training set has {} fake news and {} true news'.format((tr_df['det_fake_label'] == 1).sum(),
                                                          (tr_df['det_fake_label'] == 0).sum()))
     print('Validation set has {} fake news and {} true news'.format((dev_df['det_fake_label'] == 1).sum(),
