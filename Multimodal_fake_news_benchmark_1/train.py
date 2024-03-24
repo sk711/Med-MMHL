2b02904be866b7579b516dad7bbf49b58c0034da
sk711 <55020880+sk711@users.noreply.github.com>
2024-03-24 14:36:57 +0530

    Update train.py

--- Multimodal_fake_news_benchmark_1/train.py
+++ Multimodal_fake_news_benchmark_1/train.py
@@ -35,7 +35,7 @@ def train(train_iter, dev_dataloader, model, args):
             if args.cuda:
                 target = target.cuda()
                 mask = mask.cuda()
-                mask = mask.squeeze(1)  
+                #mask = mask.squeeze(1)  
                 print(f"Attention mask shape: {mask.shape}")
                 input_id = input_id.cuda()
 
