9006669a73664c7319c72efd96a825741224dcfc
sk711 <55020880+sk711@users.noreply.github.com>
2024-03-24 13:37:32 +0530

    Update fake_news_detection_main.py

--- Multimodal_fake_news_benchmark_1/fake_news_detection_main.py
+++ Multimodal_fake_news_benchmark_1/fake_news_detection_main.py
@@ -12,7 +12,6 @@ import MyFunc
 
 from eval import eval
 from baseline_models import bert_classifier
-from transformers import BartTokenizer
 
 
 parser = argparse.ArgumentParser(description='fk_det_model text classificer')
@@ -93,7 +92,7 @@ te_dataloader = torch.utils.data.DataLoader(
 
 # (x_train, y_train), (x_val, y_val), (x_test, y_test) = dataset.generate_data()
 # args.class_num = tr_dataset.get_class_num()
-self.tokenizer = BartTokenizer.from_pretrained(args.bert_type)
+
 
 print("\nParameters:")
 for attr, value in sorted(args.__dict__.items()):
