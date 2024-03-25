100add355289e903849ed774318f41490c804b99
sk711 <55020880+sk711@users.noreply.github.com>
2024-03-25 11:52:05 +0530

    Update bert_classifier.py

--- Multimodal_fake_news_benchmark_1/baseline_models/bert_classifier.py
+++ Multimodal_fake_news_benchmark_1/baseline_models/bert_classifier.py
@@ -1,5 +1,5 @@
 from torch import nn
-from transformers import BertModel, AutoModel, pipeline, FunnelModel, AutoModelForMaskedLM, AlbertModel, RobertaModel, DistilBertModel, BartForSequenceClassification
+from transformers import BertModel, AutoModel, pipeline, FunnelModel, AutoModelForMaskedLM, AlbertModel, RobertaModel, DistilBertModel, BartForSequenceClassification, BartModel
 
 class BertClassifier(nn.Module):
 
