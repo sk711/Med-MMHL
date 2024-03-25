09f2161286be870692cf8c3edbe4defe9002f08c
sk711 <55020880+sk711@users.noreply.github.com>
2024-03-25 11:54:31 +0530

    Update bert_classifier.py

--- Multimodal_fake_news_benchmark_1/baseline_models/bert_classifier.py
+++ Multimodal_fake_news_benchmark_1/baseline_models/bert_classifier.py
@@ -51,11 +51,11 @@ class BertClassifier(nn.Module):
         elif self.type.find('Fake_News') != -1  or self.type.find('distil') != -1 or  self.type.find('bart') != -1:
             pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)[0].mean(dim=1).squeeze()
         elif self.type.find('bart') != -1:
-          print('bart called in forward')
+          #print('bart called in forward')
         # Agar model BART hai, toh output 'logits' ke roop mein aayega
           pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)[0].mean(dim=1).squeeze()
           #print("pooled_output:", pooled_output)
-          print('BART output shape:', bart_output.shape)
+          #print('BART output shape:', bart_output.shape)
         else:
             _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False) # pooled_output: text embeeding
         # print('pooled_output', pooled_output.shape)
